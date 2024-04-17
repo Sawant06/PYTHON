e3da827f9a5c0b619e39d62b13d55b5f42a293db
Sawant06 <anamikasawant6666@gmail.com>
2024-04-17 15:51:21 +0530

    new1234

--- PATERNS.py
+++ PATERNS.py
@@ -85,23 +85,31 @@
 #             print(" ",end=" ")
 #     print()
 
-for i in range(5):
-    for j in range(7):
-         if(i==0 and j==2):
-            print(chr(65), end=" ")
-         elif(i==1 and j==1):
-             print(chr(65),end=" ")
-         elif(i==2 and j==0):
-            print(chr(65), end=" ")
-         elif(i==2 and j==2):
-            print(chr(66), end=" ")
-         elif (i == 3 and j == 1):
-            print(chr(66), end=" ")
-         elif (i == 4 and j == 2):
-             print(chr(67), end=" ")
-         else:
+# for i in range(5):
+#     for j in range(7):
+#          if(i==0 and j==2):
+#             print(chr(65), end=" ")
+#          elif(i==1 and j==1):
+#              print(chr(65),end=" ")
+#          elif(i==2 and j==0):
+#             print(chr(65), end=" ")
+#          elif(i==2 and j==2):
+#             print(chr(66), end=" ")
+#          elif (i == 3 and j == 1):
+#             print(chr(66), end=" ")
+#          elif (i == 4 and j == 2):
+#              print(chr(67), end=" ")
+#          else:
+#              print(" ",end=" ")
+#     print()
+
+for i in range(6):
+     for j in range(10):
+          if(i==0 and j==5) or (i==1 and j in{4,6}) or (i==2 and j in{3,5,7}) or (i==3 and j in{2,8}) or (i==4 and j in{1,9}):
+             print('*', end=" ")
+          else:
              print(" ",end=" ")
-    print()
+     print()
 
 
 
